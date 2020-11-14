81b9c8193eaffa1e2e5a3be32d206bc7289de773
Piotr Szlezak <piotr@szlezak.pl>
2020-11-14 15:06:21 +0100

    User registration validation

--- src/main/java/pl/sda/finalapp/app/UserController.java
+++ src/main/java/pl/sda/finalapp/app/UserController.java
@@ -6,11 +6,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Map;
+
 @Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRegistrationValidationService validationService;
 
     @GetMapping("/registration")
     public String registrationForm(Model model){
@@ -21,9 +25,17 @@ public class UserController {
     }
 
     @PostMapping("/registration")
-    public String registrationEffect(UserRegistrationDTO userRegistrationDTO){
+    public String registrationEffect(UserRegistrationDTO userRegistrationDTO, Model model){
+        final Map<String, String> validationExceptionMap = validationService.validateUser(userRegistrationDTO);
+        if(!validationExceptionMap.isEmpty()){
+            model.addAllAttributes(validationExceptionMap);
+            model.addAttribute("userRegistrationData", userRegistrationDTO);
+            model.addAttribute("countryList", Countries.values());
+            return "registrationPage";
+        }
         userService.registerUser(userRegistrationDTO);
-        return "registrationPage";
+        model.addAttribute("email", userRegistrationDTO.geteMail());
+        return "welcomePage";
     }
 
 }
---
+++ src/main/java/pl/sda/finalapp/app/UserRegistrationValidationService.java
@@ -0,0 +1,18 @@
+package pl.sda.finalapp.app;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserRegistrationValidationService {
+
+    public Map<String, String> validateUser (UserRegistrationDTO dto){
+        Map<String, String> exceptionsMap = new HashMap<>();
+        if(!dto.getFirstName().matches("^[A-Z]{1}[a-z]{2,}$")){
+            exceptionsMap.put("firstNameValidationResult", "Przynajmniej 3 znaki oraz pierwsza duża, reszta małe.");
+        }
+        return exceptionsMap;
+    }
+}
