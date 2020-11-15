0d0834009ee6ab463b87067fa2e9ae31c148c2a1
Piotr Szlezak <piotr@szlezak.pl>
2020-11-15 12:44:34 +0100

    Products page

--- src/main/java/pl/sda/finalapp/app/products/ProductController.java
+++ src/main/java/pl/sda/finalapp/app/products/ProductController.java
@@ -1,10 +1,16 @@
 package pl.sda.finalapp.app.products;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.sda.finalapp.app.categories.domain.CategoryService;
+
+import java.math.BigDecimal;
 
 @Controller
 public class ProductController {
@@ -13,10 +19,20 @@ public class ProductController {
     private CategoryService categoryService;
 
     @GetMapping("/products")
-    public String products (Model model){
+    public String products(Model model) {
         model.addAttribute("productTypesList", ProductType.values());
         model.addAttribute("categoriesList", categoryService.findAll());
         return "productsPage";
+    }
+
+    @PostMapping(value = "/products")
+    public String addProduct(@RequestParam String title,
+                             @RequestParam String description,
+                             @RequestParam String pictureUrl,
+                             @RequestParam BigDecimal price,
+                             @RequestParam ProductType productType,
+                             @RequestParam Integer categoryId) {
+        return "redirect:/products";
     }
 
 }
