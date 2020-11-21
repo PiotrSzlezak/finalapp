fd6719e073dcedbdc520e5f33f0775bcecb7f3bd
Piotr Szlezak <piotr@szlezak.pl>
2020-11-21 16:06:26 +0100

    User context

--- src/main/java/pl/sda/finalapp/app/users/RoleRepository.java
+++ src/main/java/pl/sda/finalapp/app/users/RoleRepository.java
@@ -1,10 +1,11 @@
 package pl.sda.finalapp.app.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-    //Todo - napisać ręcznie zapytanie
+    @Query("SELECT r FROM Role r WHERE lower(r.roleName)=lower(?1)")
     Role findByRoleName(String roleName);
 }
